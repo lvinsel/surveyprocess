367260686dafcf78ff9dc236466d90de8cc2093a
lvinsel <lvinsel@mps-il.com>
2015-02-04 16:01:47 -0600

    fix batch files to point to %HOMEPATH%

--- BatchFiles/ChkMtoH.bat
+++ BatchFiles/ChkMtoH.bat
@@ -1,10 +1,10 @@
 @rem = '--*-Perl-*--
 @echo off
 if "%OS%" == "Windows_NT" goto WinNT
-perl P:/MPS_Survey/perl/chkMtoH_21.pl %1 %2
+perl %HOMEPATH%/surveyprocess/chkMtoH.pl %1 %2
 goto endofperl
 :WinNT
-perl P:/MPS_Survey/perl/chkNtoH_21.pl %*
+perl %HOMEPATH%/surveyprocess/chkMtoH.pl %*
 if NOT "%COMSPEC%" == "%SystemRoot%\system32\cmd.exe" goto endofperl
 if %errorlevel% == 9009 echo You do not have Perl in your PATH.
 if errorlevel 1 goto script_failed_so_exit_with_non_zero_val 2>nul
--- BatchFiles/idot.bat
+++ BatchFiles/idot.bat
@@ -1,10 +1,10 @@
 @rem = '--*-Perl-*--
 @echo off
 if "%OS%" == "Windows_NT" goto WinNT
-perl N:/Perl/idot_8.pl %1 %2
+perl %HOMEPATH%/surveyprocess/idot.pl %1 %2
 goto endofperl
 :WinNT
-perl c:/Perl/idot_8.pl %*
+perl %HOMEPATH%/surveyprocess/idot.pl %*
 if NOT "%COMSPEC%" == "%SystemRoot%\system32\cmd.exe" goto endofperl
 if %errorlevel% == 9009 echo You do not have Perl in your PATH.
 if errorlevel 1 goto script_failed_so_exit_with_non_zero_val 2>nul
