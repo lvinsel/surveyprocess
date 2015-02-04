80fa1bdc7e00ba2e8d1aca761294fb4cc9f929f2
lvinsel <lvinsel@mps-il.com>
2015-02-04 16:35:51 -0600

    add corrected BatchFiles

--- BatchFiles/Cardno.bat
+++ BatchFiles/Cardno.bat
@@ -1,10 +1,10 @@
 @rem = '--*-Perl-*--
 @echo off
 if "%OS%" == "Windows_NT" goto WinNT
-perl N:/Perl/cardno_02.pl %1 %2
+perl %HOMEPATH%/surveyprocess/cardno.pl %1 %2
 goto endofperl
 :WinNT
-perl c:/Perl/cardno_02.pl %*
+perl %HOMEPATH%/surveyprocess/cardno.pl %*
 if NOT "%COMSPEC%" == "%SystemRoot%\system32\cmd.exe" goto endofperl
 if %errorlevel% == 9009 echo You do not have Perl in your PATH.
 if errorlevel 1 goto script_failed_so_exit_with_non_zero_val 2>nul
--- BatchFiles/MPSTOCMT.bat
+++ BatchFiles/MPSTOCMT.bat
@@ -1,10 +1,10 @@
 @rem = '--*-Perl-*--
 @echo off
 if "%OS%" == "Windows_NT" goto WinNT
-perl N:/Perl/MPStoCMT_01.pl %1 %2
+perl %HOMEPATH%/MPStoCMT.pl %1 %2
 goto endofperl
 :WinNT
-perl c:/Perl/MPStoCMT_01.pl %*
+perl %HOMEPATH%/MPStoCMT.pl %*
 if NOT "%COMSPEC%" == "%SystemRoot%\system32\cmd.exe" goto endofperl
 if %errorlevel% == 9009 echo You do not have Perl in your PATH.
 if errorlevel 1 goto script_failed_so_exit_with_non_zero_val 2>nul
