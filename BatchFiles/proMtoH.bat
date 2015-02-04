dc0f5ca21e28162a973ab5290f2cb82a20f3cb1a
lvinsel <lvinsel@mps-il.com>
2015-02-04 16:31:09 -0600

    add corrected BatchFiles

--- BatchFiles/proMtoH.bat
+++ BatchFiles/proMtoH.bat
@@ -1,10 +1,10 @@
 @rem = '--*-Perl-*--
 @echo off
 if "%OS%" == "Windows_NT" goto WinNT
-perl P:/MPS_Survey/perl/proMtoH_19.pl %1 %2
+perl %HOMEPATH%/surveyprocess/proMtoH.pl %1 %2
 goto endofperl
 :WinNT
-perl P:/MPS_Survey/perl/proMtoH_19.pl %*
+perl %HOMEPATH%/surveyprocess/proMtoH.pl %*
 if NOT "%COMSPEC%" == "%SystemRoot%\system32\cmd.exe" goto endofperl
 if %errorlevel% == 9009 echo You do not have Perl in your PATH.
 if errorlevel 1 goto script_failed_so_exit_with_non_zero_val 2>nul
