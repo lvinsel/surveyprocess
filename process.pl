1051a066653398b8b43963024d6ee397871348da
lvinsel <lvinsel@mps-il.com>
2015-03-03 11:45:21 -0600

    move linecode to correct postion

--- process.pl
+++ process.pl
@@ -1577,7 +1577,7 @@ while (<IN>) {
  #################################################
    if ($c = $idotcommands{$tok[0]})
      {
-     print OUT1 "$in[0],$in[1],$in[2],$in[3],$Icode$csplit[1]$fsplit[1]$c\n";
+     print OUT1 "$in[0],$in[1],$in[2],$in[3],$Icode$csplit[1]$c$fsplit[1]\n";
      }
    else
      {
